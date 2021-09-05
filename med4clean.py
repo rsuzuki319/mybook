37827f1c51f1bb7e4b8cd4ac666e45a54b713151
higenobu <33110183+higenobu@users.noreply.github.com>
2021-09-05 16:31:00 -0700

    Update med4clean.py

--- med4clean.py
+++ med4clean.py
@@ -2,13 +2,6 @@
 # -*- coding: utf-8 -*-
 import sys
 import csv
-#import pprint
-
-#import mojimoji
-
-#from textformatting import ssplit
-#from pyknp import Juman
-
 args = sys.argv
 #print('args1:[{}]'.format(args[1]))
 #jumanpp = Juman(command='/home/ubuntu/local/bin/jumanpp')
@@ -17,15 +10,7 @@ with open(args[1]) as f:
     reader = csv.reader(f, delimiter=',')
     ls = [row for row in reader]
 
-#    r = ls[4]
-#    print(r)
-#    sentences = ssplit(ls[4][0])
-#    print('step1 : {}'.format(sentences))
 
-#    for i in range(len(l)):        
-#        print('step1-{} : {}'.format(i,l[i][0]))
-#        sentences = ssplit(l[i][0])
-    #print (ls)
     result_ls = []
     result_rows=[]
     tags=[]
@@ -46,7 +31,7 @@ with open(args[1]) as f:
 
         sentences = cont.split('。')
         for ss in sentences:
-            #print (ss)
+            
             if (ss==''):
                 continue
         
@@ -58,12 +43,12 @@ with open(args[1]) as f:
     
 
     for i in range(len(result_ls)):
-#        print('[{}][{}]'.format(result_ls[i],ls[i][1]))
+
         result_rows.append([ result_ls[i][0],result_ls[i][1]])
         result_rows.append([ '',result_ls[i][1]])
     
     
-    #print(result_rows[0])
+    
 
 with open(args[2], 'w') as f:
     writer = csv.writer(f, delimiter='\t')
